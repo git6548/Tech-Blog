ba993f2518eb956e67b03f372914768f46d59821
Stephanie <stephanieanderson5133@gmail.com>
2022-03-23 16:25:17 -0500

    initial setup

---
+++ server.js
@@ -0,0 +1,25 @@
+// note I may need to update parts of this
+
+// Dependencies
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const hbs = exphbs.create({});
+
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+// I need to update this for my needs
+app.use(require('./controllers/dish-routes'));
+
+// Starts the server to begin listening
+app.listen(PORT, () => {
+  console.log('Server listening on: http://localhost:' + PORT);
+});
